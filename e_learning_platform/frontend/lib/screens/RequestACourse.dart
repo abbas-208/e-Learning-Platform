aa3c05ac1d56e9f100df3f2dbf728fbcfdf60ecf
TheSaqibShehzad <shehzadsaqib424@gmail.com>
2023-11-07 20:59:05 +0500

    Request a Course screen completed

--- e_learning_platform/frontend/lib/screens/RequestACourse.dart
+++ e_learning_platform/frontend/lib/screens/RequestACourse.dart
@@ -127,7 +127,99 @@ class _RequestACourseState extends State<RequestACourse> {
                           ),
                         ),
                       ),
-                      
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          controller: courseUrlController,
+                          keyboardType: TextInputType.multiline,
+                          decoration: InputDecoration(
+                            labelText: "Course URL",
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.white, width: 2.0),
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.white, width: 2.0),
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: DropdownButton(
+                          hint: Text('Choose a Category'),
+                          value: _selectedLocation,
+                          onChanged: (newValue) {
+                            setState(() {
+                              _selectedLocation = newValue;
+                            });
+                          },
+                          items: Provider.of<CourseProvider>(context,
+                                  listen: false)
+                              .cd
+                              .map((location) {
+                            return DropdownMenuItem(
+                              child: new Text(location.categoryName),
+                              value: location.id,
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: SizedBox(
+                  width: 250,
+                  height: 45,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.indigoAccent,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                      onPressed: utils.uploadImage,
+                      child: Text(
+                          utils.userimage.isEmpty
+                              ? "Upload Course Image"
+                              : "Reselect Image",
+                          style: TextStyle(color: Colors.white))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: SizedBox(
+                  width: 250,
+                  height: 45,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(158, 17, 244, 240),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                      onPressed: () async {
+                        Provider.of<CourseProvider>(context, listen: false)
+                            .addCourseReq(
+                                courseNameController.text,
+                                courseDescriptionController.text,
+                                utils.userimage,
+                                channeNameController.text,
+                                courseUrlController.text,
+                                _selectedLocation)
+                            .whenComplete(() => {
+                                  courseNameController.clear(),
+                                  courseUrlController.clear(),
+                                  channeNameController.clear(),
+                                  courseDescriptionController.clear(),
+                                });
+                      },
+                      child: Text("Add Course",
+                          style: TextStyle(color: Colors.white))),
+                ),
+              ),
             ],
           ),
         ),
