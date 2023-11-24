b3359cde6cbd48bfde39b77ac163c6001cbdfafa
TheSaqibShehzad <shehzadsaqib424@gmail.com>
2023-11-24 21:01:04 +0500

    Image Utility services added in Image and Utility class using cloudinary

---
+++ e_learning_platform/frontend/lib/Credentials.dart
@@ -0,0 +1,5 @@
+class Cred {
+  static const String APIKEY = '588824372867367';
+  static const String APISecret = 'cYvSDfHL_Iswq6LI_fhpow1xX6U';
+  static const String Cloud = 'dytz4tuqy';
+}
---
+++ e_learning_platform/frontend/lib/Services/Api.dart
@@ -0,0 +1,7 @@
+class API {
+  var api = "https://localhost:4000";
+
+  // var api = "https://courseriver.cyclic.app";
+  // var api =
+  // 'mongodb+srv://saqib:saqib@cluster0.kllgvig.mongodb.net/?retryWrites=true&w=majority';
+}
